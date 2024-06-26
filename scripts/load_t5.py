0d9c1bb17ec013c1fc4a70c75461dc54951c8348
Lazy Dart <estp.pl@gmail.com>
2024-06-26 16:19:08 +0200

    More details related to using this script

--- scripts/load_t5.py
+++ scripts/load_t5.py
@@ -7,17 +7,16 @@ from transformers import T5Tokenizer, T5ForConditionalGeneration
 from argparse import ArgumentParser
 
 
-def download_plt5(base=True, colab=False):
+def download_plt5(large=False, colab=False):
 
+    kind = "large" if large else "base"
     root = "../" if not colab else "./poleval-2024-qa/"
 
-    model_folder = os.path.join(root, 'models')
+    model_folder = os.path.join(root, f'models/plt5-original-{kind}')
 
     # Create directory if not present
     if not os.path.exists(model_folder):
         os.makedirs(model_folder)
-
-    kind = "base" if base else "large"
 
     # Load T5 Model
     tokenizer = T5Tokenizer.from_pretrained(f'allegro/plt5-{kind}')
@@ -27,9 +26,19 @@ def download_plt5(base=True, colab=False):
     tokenizer.save_pretrained(model_folder)
     model.save_pretrained(model_folder)
 
+
 if __name__ == "__main__":
+
+    # Running it from command line
+    # On Local Machine
+    # cd to scripts folder
+    # python load_t5.py
+
+    # On Colab
+    # !python ./poleval-2024-qa/scripts/load_t5.py --colab
+
     parser = ArgumentParser()
-    parser.add_argument("--base", action="store_true", help="Download base model")
+    parser.add_argument("--large", action="store_true", help="Download base model")
     parser.add_argument("--colab", action="store_true", help="Running on Colab")
 
     args = parser.parse_args()
